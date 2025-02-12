e731c7a92a8aa027af562e1ee40e6e56a26afe3e
Maeva-B <82147604+Maeva-B@users.noreply.github.com>
2025-02-12 17:28:32 +0100

    update id and init.js

--- books-api/app/endpoints/loans.py
+++ books-api/app/endpoints/loans.py
@@ -22,27 +22,23 @@ async def get_loan(loan_id: str):
     loan = await loans_collection.find_one({"_id": oid})
     if loan:
         loan["id"] = str(loan["_id"])
+        loan["book_id"] = str(loan["book_id"])
+        loan["adherent_id"] = str(loan["adherent_id"])
         return loan
     raise HTTPException(status_code=404, detail="Loan not found")
+
 
 @router.get("/", response_model=List[Loan])
 async def get_loans(
     loanDate: Optional[str] = None,
     returnDate: Optional[str] = None,
     book_id: Optional[str] = None,
-    adherent_id: Optional[str]= None,
+    adherent_id: Optional[str] = None,
     skip: int = 0,
     limit: int = 10
 ):
     """
-    Retrieves all loans with optional filtering by loan date, return date, book id and adherent id, with pagination.
-
-    Example URL:
-      GET http://localhost/loans?loanDate=2024-10-06&returnDate=2024-12-30&skip=0&limit=10
-      
-    Skip (default 0): Indicates the number of loans to skip from the beginning of the result set.
-    
-    Limit (default 10): Indicates the maximum number of loans to return.
+    Retrieves all loans with optional filtering.
     """
     query = {}
     if loanDate:
@@ -57,58 +53,36 @@ async def get_loans(
     loans_cursor = loans_collection.find(query).skip(skip).limit(limit)
     loans = await loans_cursor.to_list(length=limit)
     for loan in loans:
-        # Convert the MongoDB ObjectId to string and assign it to 'id'
         loan["id"] = str(loan["_id"])
+        loan["book_id"] = str(loan["book_id"])
+        loan["adherent_id"] = str(loan["adherent_id"])
     if loans:
         return loans
     raise HTTPException(status_code=404, detail="No loans found")
 
+
 @router.post("/", response_model=Loan, status_code=status.HTTP_201_CREATED)
-async def create_loan(loan : LoanCreate):
+async def create_loan(loan: LoanCreate):
     """
     Creates a new loan.
-
-    Example URL:
-      POST http://localhost/loans
-
-    Example payload:
-      {
-          "loanDate": "2025-04-20",
-          "returnDate": "2025-04-27",
-          "book_id": "2",
-          "adherent_id": "0"
-      }
     """
-    
     loan_doc = loan.dict()
     loan_doc['loanDate'] = loan_doc['loanDate'].isoformat()
     loan_doc['returnDate'] = loan_doc['returnDate'].isoformat()
     result = await loans_collection.insert_one(loan_doc)
-    # Append the generated id to the document.
     loan_doc["id"] = str(result.inserted_id)
     return loan_doc
+
 
 @router.put("/{loan_id}", response_model=Loan, status_code=status.HTTP_200_OK)
 async def update_loan(loan_id: str, loan: LoanCreate):
     """
-    Updates an existing loan
-
-    Example URL: PUT http://localhost/loans/67a391a4198cd394f628c25f
-
-    Example payload:
-    {
-        "loanDate": "2025-04-20",
-        "returnDate": "2025-04-27",
-        "book_id": "2",
-        "adherent_id": "0"
-    }
+    Updates an existing loan.
     """
-    # Retrieve the loan id
     try:
         oid = ObjectId(loan_id)
     except Exception:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid loan ID format")
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid loan ID format")
 
     loan_doc = loan.dict()
     loan_doc['loanDate'] = loan_doc['loanDate'].isoformat()
@@ -119,62 +93,56 @@ async def update_loan(loan_id: str, loan: LoanCreate):
         updated_loan = await loans_collection.find_one({"_id": oid})
         if updated_loan:
             updated_loan["id"] = str(updated_loan["_id"])
+            updated_loan["book_id"] = str(updated_loan["book_id"])
+            updated_loan["adherent_id"] = str(updated_loan["adherent_id"])
             return updated_loan
 
-    # Check if the loan exists; if not, raise a 404 error.
     existing_loan = await loans_collection.find_one({"_id": oid})
     if not existing_loan:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="Loan not found")
-
-    # If no modifications were made, return the existing loan.
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Loan not found")
+    
     existing_loan["id"] = str(existing_loan["_id"])
+    existing_loan["book_id"] = str(existing_loan["book_id"])
+    existing_loan["adherent_id"] = str(existing_loan["adherent_id"])
     return existing_loan
+
 
 @router.delete("/{loan_id}", status_code=status.HTTP_200_OK)
 async def delete_loan(loan_id: str):
     """
     Deletes an existing loan.
-    Example URL: DELETE http://localhost/loans/67a391a4198cd394f628c25f
     """
     try:
         oid = ObjectId(loan_id)
     except Exception:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid loan ID format")
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid loan ID format")
 
     result = await loans_collection.delete_one({"_id": oid})
     if result.deleted_count == 1:
-        return  # HTTP 204 No Content
-    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                        detail="Loan not found")
+        return
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Loan not found")
+
 
 @router.delete("/", response_model=int, status_code=status.HTTP_200_OK)
 async def delete_all_loan(
     loanDate: Optional[str] = None,
     returnDate: Optional[str] = None,
     book_id: Optional[str] = None,
-    adherent_id: Optional[str]= None
+    adherent_id: Optional[str] = None
 ):
     """
-    Deletes all existing loan selecting by loan date, return date, book id and/or adherent id.
-    Example URL:
-      DELETE
-        http://localhost/loans?loanDate=2024-10-06&returnDate=2024-12-30&book_id=0&adherent_id=10
-    
-    Returns the deleted loans number.
+    Deletes all loans matching the provided filters.
+    Returns the number of deleted loans.
     """
     query = {}
-    if loanDate :
-        if re.match("^[0-9]{4}-[0-1][0-9]-[0-9]{2}$", "{loanDate}" ):
-            raise HTTPException(
-                status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid loan date format")
+    if loanDate:
+        if re.match("^[0-9]{4}-[0-1][0-9]-[0-9]{2}$", f"{loanDate}"):
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid loan date format")
         query["loanDate"] = loanDate
 
-    if returnDate :
-        if re.match("^[0-9]{4}-[0-1][0-9]-[0-9]{2}$", "{returnDate}" ):
-            raise HTTPException(
-                status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid return date format")
+    if returnDate:
+        if re.match("^[0-9]{4}-[0-1][0-9]-[0-9]{2}$", f"{returnDate}"):
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid return date format")
         query["returnDate"] = returnDate
     
     if book_id:
@@ -186,5 +154,4 @@ async def delete_all_loan(
     result = await loans_collection.delete_many(query)
     if result.deleted_count >= 1:
         return result.deleted_count
-    print("Loans not found")
     return 0
